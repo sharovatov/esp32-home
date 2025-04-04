8b8c6f812486734855ecf210bed65c3cbae2733f
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2025-04-04 17:29:26 +0200

    refactored fake mqtt client out the main tests code file

---
+++ include/fake_mqtt_client.h
@@ -0,0 +1,38 @@
+
+#pragma once
+
+#include "mqtt_client.h"
+#include <string>
+
+class FakeMqttClient : public MqttClient
+{
+public:
+    void publish(const std::string &topic, const std::string &message) override
+    {
+        lastTopic = topic;
+        lastMessage = message;
+    }
+
+    void subscribe(const std::string &topic) override
+    {
+        // No-op for now
+    }
+
+    void setCallback(void (*callback)(char *, uint8_t *, unsigned int)) override
+    {
+        // No-op for now
+    }
+
+    void loop() override
+    {
+        // No-op for now
+    }
+
+    bool connect(const char *clientId) override
+    {
+        return true; // Assume always connected for test double
+    }
+
+    std::string lastTopic;
+    std::string lastMessage;
+};
