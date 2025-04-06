e4b04db5cb66d90f087d35c1b9418d8360a9cff0
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2025-04-06 18:13:55 +0200

    I decided that since esp has quite a small number of available gpios, the number of sensors I can add is very small too. So there's little need in sensor.js generation

--- tools/sensors_js_generator.py
+++
@@ -1,6 +0,0 @@
-def generate_js(sensors):
-    lines = ['// Auto-generated file', '', 'export const availableSensors = [']
-    for s in sensors:
-        lines.append(f'  "{s}",')
-    lines.append('];')
-    return '\n'.join(lines)
